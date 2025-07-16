cae4279c4a520b6ab07269a1a8067ece7e8c591a
zloverty <liuzy19911112@gmail.com>
2025-07-16 13:16:27 +0800

    cli logging config for setting debug log

--- bubble_bouncing/cli.py
+++ bubble_bouncing/cli.py
@@ -19,30 +19,40 @@ from bubble_bouncing import SimulationParams
 from bubble_bouncing import BubbleDataVisualizer
 import logging
 
-def run(save_folder, args_dict, exist_ok):
-    params = SimulationParams(**args_dict)
-    sim = BounceSimulator(save_folder, params, exist_ok=exist_ok)
-    logging.basicConfig(
-        filename = sim.log_file,
-        level = logging.INFO,        # Set the logging level
-        format = '%(asctime)s - %(levelname)s - %(message)s',  # Log format
-    )
-    sim.run()
+test_folder = "~/Documents/.bcsim_test"
 
 def main():
     parser = argparse.ArgumentParser(f"This script takes in the save_folder as a positional argument. Optional arguments can be passed to set simulation parameters. Available arguments are {available_keys()}. Set using --arg ARG pairs.")
-    parser.add_argument("--save_folder", type=str, default="~/Documents/test", help="folder to save simulation data.")
+    parser.add_argument("--save_folder", type=str, default=test_folder, help="folder to save simulation data.")
     parser.add_argument("-f", action="store_true")
+    parser.add_argument('--log-level', '-l',
+                        default='INFO',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help='Set the logging level (default: INFO)')
     # Parse known args; leave unknown ones (like --R, --T, etc.)
     args, unknown = parser.parse_known_args()
     save_folder = Path(args.save_folder).expanduser().resolve()
     args_dict = parse_params(unknown)
+    params = SimulationParams(**args_dict)
+    log_level_str = args.log_level
+    sim = BounceSimulator(save_folder, params, exist_ok=args.f)
+    
+    
+    numeric_level = getattr(logging, log_level_str.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {log_level_str}")
 
-    run(save_folder, args_dict, args.f)
+    logging.basicConfig(
+        filename = sim.log_file,
+        level = numeric_level,        # Set the logging level
+        format = '%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    )
+    sim.run()
+    
 
 def view_traj():
     parser = argparse.ArgumentParser(f"Bubble visualizer.")
-    parser.add_argument("--folder", type=str, default="~/Documents/test", help="Main data folder.")
+    parser.add_argument("--folder", type=str, default=test_folder, help="Main data folder.")
     parser.add_argument("--mode", type=str, default="none", help="Visualization mode: s, v or vh.")
     args = parser.parse_args()
     vis = BubbleDataVisualizer(args.folder)
@@ -50,7 +60,7 @@ def view_traj():
 
 def view_morphology():
     parser = argparse.ArgumentParser(f"Bubble visualizer.")
-    parser.add_argument("--folder", type=str, default="~/Documents/test", help="Main data folder.")
+    parser.add_argument("--folder", type=str, default=test_folder, help="Main data folder.")
     parser.add_argument("--mode", type=str, default="none", help="Visualization mode: s, v or vh.")
     args = parser.parse_args()
     vis = BubbleDataVisualizer(args.folder)
@@ -58,7 +68,7 @@ def view_morphology():
 
 def view_oseen():
     parser = argparse.ArgumentParser(f"Bubble visualizer.")
-    parser.add_argument("--folder", type=str, default="~/Documents/test", help="Main data folder.")
+    parser.add_argument("--folder", type=str, default=test_folder, help="Main data folder.")
     parser.add_argument("--mode", type=str, default="none", help="Visualization mode: s, v or vh.")
     args = parser.parse_args()
     vis = BubbleDataVisualizer(args.folder)
